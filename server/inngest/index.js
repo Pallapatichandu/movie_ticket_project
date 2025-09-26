978239ea1deed70105392e8d25a6116a95429821
Pallapatichandu <pallapatichandu41@gmail.com>
2025-09-26 20:52:42 +0530

    index file update

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
-import User from "../models/User.js";
+import User from "../models/User";
+// import User from "../models/User.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
@@ -15,12 +16,12 @@ const syncUserCreation = inngest.createFunction(
 
     const userData = {
       _id: id, // Ensure your schema supports String IDs
-      email: email_addresses?.[0]?.email_address,
-      name: [first_name, last_name].filter(Boolean).join(" "),
+      email: email_addresses[0].email_address,
+      name: first_name + " " + last_name,
       image: image_url,
     };
 
-    await User.create(userData);
+   await User.create(userData)
   }
 );
 
@@ -39,20 +40,21 @@ const syncUserUpdation = inngest.createFunction(
   { id: "update-user-with-clerk" },
   { event: "clerk/user.updated" },
   async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+   const{id, first_name, last_name, email_addresses, image_url}=event.data
+   const userData={
+    _id:id,
+    email:email_addresses[0].email_address,
+    name:first_name + " " + last_name,
+    image:image_url
 
-    const userData = {
-      email: email_addresses?.[0]?.email_address,
-      name: [first_name, last_name].filter(Boolean).join(" "),
-      image: image_url,
-    };
-
-    await User.findByIdAndUpdate(id, userData, { new: true, upsert: true });
   }
+  await User.findByIdAndUpdate(id,userData)
+  }
+  
 );
 
 export const functions = [
-  syncUserCreation,
+ syncUserCreation,
   syncUserDeletion,
-  syncUserUpdation,
+  syncUserUpdation
 ];
