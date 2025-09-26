cccd7cb33458cee0724fc2758cb6b85c3bac7b24
Pallapatichandu <pallapatichandu41@gmail.com>
2025-09-26 17:04:27 +0530

    mongoose fix correct

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,57 +1,58 @@
 import { Inngest } from "inngest";
+import User from "../models/User.js";
+
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
-// Create an empty array where we'll export future Inngest functions
-const syncUserCreation=inngest.createFunction(
-    {
-        id:'sync-user-from-clerk'
-    },
-    {
-        event:"clerk/user.created"
-    },
-    async({event})=>{
-        const{id,first_name,last_name,email_addresses,image_url}=event.data
-        const userData={
-            _id:id,
-            email:email_addresses[0].email_address,
-            name:first_name + " " +last_name,
-            image:image_url
-        }
-        await User.create(userData)
-    }
-)
 
-const syncUserDeletion=inngest.createFunction(
-    {
-        id:'delete-user-with-clerk'
-    },
-    {
-        event:"clerk/user.deleted"
-    },
-    async({event})=>{
-        const{id}=event.data
-       await User.findByIdAndDelete(id)
-       
-    }
-)
+// Clerk → MongoDB Sync Functions
 
-const syncUserUpdation=inngest.createFunction(
-    {
-        id:'update-user-with-clerk'
-    },
-    {
-        event:"clerk/user.updated"
-    },
-    async({event})=>{
-       const{id,first_name,last_name,email_addresses,image_url}=event.data
-        const userData={
-            _id:id,
-            email:email_addresses[0].email_address,
-            name:first_name + " " +last_name,
-            image:image_url
-        }
-        await User.findByIdAndUpdate(id,userData)
-       
-    }
-)
-export const functions = [syncUserCreation,syncUserDeletion,syncUserUpdation];
+// 1. User Created
+const syncUserCreation = inngest.createFunction(
+  { id: "sync-user-from-clerk" },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+    const userData = {
+      _id: id, // Ensure your schema supports String IDs
+      email: email_addresses?.[0]?.email_address,
+      name: [first_name, last_name].filter(Boolean).join(" "),
+      image: image_url,
+    };
+
+    await User.create(userData);
+  }
+);
+
+// 2. User Deleted
+const syncUserDeletion = inngest.createFunction(
+  { id: "delete-user-with-clerk" },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    const { id } = event.data;
+    await User.findByIdAndDelete(id);
+  }
+);
+
+// 3. User Updated
+const syncUserUpdation = inngest.createFunction(
+  { id: "update-user-with-clerk" },
+  { event: "clerk/user.updated" },
+  async ({ event }) => {
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
+
+    const userData = {
+      email: email_addresses?.[0]?.email_address,
+      name: [first_name, last_name].filter(Boolean).join(" "),
+      image: image_url,
+    };
+
+    await User.findByIdAndUpdate(id, userData, { new: true, upsert: true });
+  }
+);
+
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+];
--- server/models/User.js
+++ server/models/User.js
@@ -1,10 +1,10 @@
-import mangoose from 'mangoose'
-const userSchema=new mangoose.Schema({
+import mongoose from "mongoose";
+const userSchema=new mongoose.Schema({
     _id:{type:String,required:true},
     name:{type:String,required:true},
     email:{type:String,required:true},
     image:{type:String,required:true},
 
 })
-const User=mangoose.model('User',userSchema)
+const User=mongoose.model('User',userSchema)
 export default User
