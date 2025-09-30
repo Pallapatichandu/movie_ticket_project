2eeb9aba4547c9aa5f33523addb2ce92ceac4cc0
Pallapatichandu <pallapatichandu41@gmail.com>
2025-09-30 23:59:54 +0530

    update stripe webhooks

--- server/models/show.js
+++ server/models/show.js
@@ -1,8 +1,10 @@
+
+
 import mongoose from "mongoose";
 
 const showSchema = new mongoose.Schema(
   {
-    movie: { type: mongoose.Schema.Types.ObjectId, ref: "Movie", required: true },
+    movie: { type: String, required: true, ref: "Movie" }, // must match model name
     showDateTime: { type: Date, required: true },
     showPrice: { type: Number, required: true },
     occupiedSeats: { type: Object, default: {} },
